fbb83c28b28573735243025e0937eab58c0c69b8
Tyler Poland <tyler.poland@gmail.com>
2017-04-26 12:47:57 -0700

    version 0.0.1

--- lib/recollect/recollect.rb
+++ lib/recollect/recollect.rb
@@ -1,13 +1,13 @@
-require 'FileUtils'
+require 'fileutils'
 
 module Recollect
   class Recollection
     def initialize(args)
-      unless args.length >= 1 && args.length <= 2
-        usage
-        exit
-      end
+      @reserved = %w[new edit remove help search]
+      usage unless args.length >= 1 && args.length <= 2
       @action, @name = args
+      usage if @reserved.include?(@action) && args.length < 2
+      usage if args.length == 2 && @reserved.include?(@name)
       @separator = '-------------------------'
       @recollect_path = File.join(ENV['HOME'], '.recollections')
       FileUtils.mkdir_p(@recollect_path)
@@ -15,17 +15,18 @@ module Recollect
 
     def usage(msg = nil)
       puts msg unless msg.nil?
-      puts 'USAGE: #{__FILE__} ACTION [arg]'
+      puts "USAGE: recollect ACTION [arg]"
       puts '  Actions: list, new, edit, or <name> where <name> is the recollection to be displayed.'
       puts '    list        - provides a listing of all available recollections'
       puts "    name        - displays the recollection matching 'name'"
       puts '    new [name]  - provides a listing of all available recollections'
       puts '    edit [name] - provides a listing of all available recollections'
       puts '    remove [name]   - removes a recollections'
+      puts "Note: #{@reserved} are reserved and cannot be the name of a recollection."
       exit 1
     end
 
-    def get_recollections
+    def recollection_names
       recollections.map { |r| File.basename(r, File.extname(r)) }
     end
 
@@ -37,7 +38,6 @@ module Recollect
       unless item_exists?(item)
         puts "Unable to find a recollecter matching '#{item}'"
         usage
-        exit
       end
     end
 
@@ -46,7 +46,39 @@ module Recollect
     end
 
     def write_file
-      `mate #{File.join(@recollect_path, @name) + '.txt'}`
+      `#{ENV['EDITOR']} #{File.join(@recollect_path, @name) + '.txt'}`
+    end
+
+    def list_recollections
+      puts "Available recollections:\n#{@separator}"
+      puts recollection_names.map { |r| '  ' + r }
+    end
+
+    def new_recollection
+      msg = "'#{@name}' Already exists; use edit to change."
+      abort msg if item_exists?(@name)
+      write_file
+    end
+
+    def edit_recollection
+      verify_name
+      write_file
+    end
+
+    def remove_recollection
+      verify_name
+      File.delete(File.join(@recollect_path, @name) + '.txt') if confirm?
+    end
+
+    def print_recollection
+      verify_name(@action)
+      puts @separator
+      #puts `cat #{File.join(@recollect_path, @action) + '.*'}`
+      File.open(File.join(@recollect_path, @action) + '.txt', 'r') do |f|
+        f.each_line do |line|
+          puts line
+        end
+      end
     end
 
     def confirm?
@@ -58,22 +90,15 @@ module Recollect
     def run
       case @action
       when 'list'
-        puts "Available recollections:\n#{@separator}"
-        puts get_recollections.map { |r| '  ' + r }
+        list_recollections
       when 'new'
-        msg = "'#{@name}' Already exists; use edit to change."
-        abort msg if item_exists?(@name)
-        write_file
+        new_recollection
       when 'edit'
-        verify_name
-        write_file
+        edit_recollection
       when 'remove'
-        verify_name
-        File.delete(File.join(@recollect_path, @name) + '.txt') if confirm?
+        remove_recollection
       else
-        verify_name(@action)
-        puts @separator
-        puts `cat #{File.join(@recollect_path, @action) + '.*'}`
+        print_recollection
       end
     end
   end
--- recollect.gemspec
+++ recollect.gemspec
@@ -10,14 +10,11 @@ Gem::Specification.new do |s|
   s.summary = "Storage, Management and Retrieval of things you'd like to remember (recollect)."
   s.description = s.summary
   s.platform = Gem::Platform::RUBY
-  s.required_ruby_version = '~> 2.4'
-  # s.required_rubygems_version = Gem::Requirement.new("> 1.3.1") if s.respond_to?(:required_rubygems_version=)
+  s.required_ruby_version = '~> 1.9'
   s.has_rdoc = true
   s.extra_rdoc_files = %w[LICENSE TODO]
-  #s.files = %w[LICENSE README.md Rakefile TODO] + Dir.glob('lib/**/*')
-  # s.test_files = Dir.glob("spec/**/*")
   s.bindir = 'bin'
   s.executables = %w[recollect]
   s.default_executable = 'recollect'
-  s.files = `git ls-files`.split('\n')
+  s.files = `git ls-files`.split("\n")
 end
